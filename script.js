bbab3fe03e07d4d667bb551d982909511350ae81
Ian Cunningham <35973428+icunningham7@users.noreply.github.com>
2022-07-31 10:31:38 -0600

    Add Check to ensure 2 or more character sets are chosen

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordRequirements = {
-  passwordLength: 8,
+var passwordSettings = {
+  passwordLength: 12,
+  passwordLower: false,
+  passwordUpper: false,
+  passwordNumeric: false,
+  passwordSpecial: false
 };
 
 // Write password to the #password input
@@ -14,22 +18,33 @@ function writePassword() {
 }
 
 function generatePassword() {
-  
-  console.log("Ran generatePassword");
+  getPasswordSettings();
 
-  //Get Length for the Password
-  getPasswordLength();
+  console.log("Your password settings passed validation. Need to implement actual password generation");
+};
   
+// Get Password Settings
+
+function getPasswordSettings() {
+
+  console.log("Getting Password Settings");
+
+  settingsReset();
+
+  setLength();
+  setLowercase();
+  setUppercase();
+  setNumeric();
+  setSpecial();
+  
+  validatePasswordSettings();
 
   console.log("Finished generatePassword");
 }
 
-// Get Password Requirements
 
-function getPasswordLength() {
-  
-  console.log("Ran getPasswordLength");
 
+function setLength() {
   let pwLength = prompt("Password length (8-128 characters)", 12);
 
   while (!(pwLength >= 8 && pwLength <= 128)) {
@@ -37,8 +52,77 @@ function getPasswordLength() {
     alert("Please enter a valid number between 8 and 128");
     pwLength = prompt("Password length (8-128 characters)", 12);
   };
-  passwordRequirements.passwordLength = pwLength;
-  return passwordRequirements.passwordLength;
+
+  passwordSettings.passwordLength = pwLength;
+  return passwordSettings.passwordLength;
+};
+
+function setLowercase() {
+
+  passwordSettings.passwordLower = confirm("Do you want to include lowercase letters?");
+  
+  return passwordSettings.passwordLower;
 }
+
+function setUppercase() {
+
+  passwordSettings.passwordUpper = confirm("Do you want to include uppercase letters?");
+
+  console.log(`passwordUpper is set to ` + passwordSettings.passwordUpper);
+  console.log(`passwordUpper typeof is ` + (typeof passwordSettings.passwordUpper));
+
+  return passwordSettings.passwordUpper;
+}
+
+function setNumeric() {
+
+  passwordSettings.passwordNumeric = confirm("Do you want to include numbers?");
+
+  return passwordSettings.passwordNumeric;
+}
+
+function setSpecial() {
+
+  passwordSettings.passwordSpecial = confirm("Do you want to include special characters?");
+
+  return passwordSettings.passwordSpecial;
+}
+
+// Reset User Settings
+
+function settingsReset() {
+  passwordSettings = {
+    passwordLength: 0,
+    passwordLower: false,
+    passwordUpper: false,
+    passwordNumeric: false,
+    passwordSpecial: false
+  };
+  console.log("Reset all user choices.")
+};
+
+// Validate Password Parameters - 2 or more character groups must be chosen
+
+function validatePasswordSettings() {
+  let pwSettingsCounter = 0;
+  for (property in passwordSettings) {
+    console.log(property + "has a type of " + (typeof (passwordSettings[property])) + " and has a value of " + passwordSettings[property]);
+    if ((passwordSettings[property]) === true) {
+      pwSettingsCounter++;
+      console.log(pwSettingsCounter);
+    };
+  };
+
+  if (pwSettingsCounter < 2) {
+    alert("Please select 2 or more character sets.");
+    // generatePassword();
+  } else {
+    console.log(`Your generated password is strong. It has ${pwSettingsCounter} character sets.`);
+    return;
+  }
+  return;
+};
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+

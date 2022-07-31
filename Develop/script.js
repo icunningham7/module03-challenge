069d8cbcbc6f5973307e4f38fc6f4647b68a7f9f
Ian Cunningham <35973428+icunningham7@users.noreply.github.com>
2022-07-30 23:06:17 -0600

    Add password length selector

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordRequirements = {
+  passwordLength: 8,
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +13,25 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  
+  console.log("Ran generatePassword");
+
+  //Get Length for the Password
+  getPasswordLength();
+  while (!passwordRequirements >= 8 && !passwordRequirements <= 128) {
+    
+    alert("Please enter a valid number between 8 and 128");
+    getPasswordLength();
+  }
+}
+
+// Get Password Requirements
+
+function getPasswordLength() {
+  console.log("Ran getPasswordLength");
+  passwordRequirements.passwordLength  = prompt("Password length (8-128 characters", 12);
+  return passwordRequirements.passwordLength;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
